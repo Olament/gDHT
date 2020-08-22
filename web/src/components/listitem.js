c6e6286d321752b1159d0285eac2973153593690
Zixuan Guo <i@zxguo.me>
2020-08-22 02:30:25 -0500

    add autosuggest

--- web/src/components/listitem.js
+++ web/src/components/listitem.js
@@ -3,14 +3,14 @@ import React from "react";
 export default class ListItem extends React.Component {
     render() {
         return (
-            <div className="row">
-                <div className="cell">
+            <div className="row" style={{maxWidth: "90%"}}>
+                <div className="cell" style={{overflowWrap: "break-word"}}>
                     {this.props.name}
                 </div>
                 <div className="cell" style={{justifyContent: "center"}}>
                     {this.props.size}
                 </div>
-                <div className="cell" style={{direction: "rtl"}}>
+                <div className="cell" style={{justifyContent: "center"}}>
                     <a href={'magnet:?xt=urn:btih:' + this.props.link}>Link</a>
                 </div>
             </div>
--- web/src/components/searchbar.js
+++ web/src/components/searchbar.js
@@ -1,27 +1,86 @@
 import React from 'react'
+import Autosuggest from 'react-autosuggest';
+
+
+const getSuggestionValue = suggestion => suggestion;
+
+const renderSuggestion = suggestion => (
+    <div>
+        {suggestion}
+    </div>
+);
 
 export default class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             value: '',
-            suggest: []
+            suggestions: []
+        };
+    }
+
+    onChange = (event, { newValue, method }) => {
+        this.props.updateQueryText(newValue);
+        console.log(method);
+        if (method === 'enter') {
+            this.props.handleEnter();
         }
+        this.setState({
+            value: newValue
+        });
+    };
+
+    onSuggestionsFetchRequested = ({ value }) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "suggest": {
+                    "search-suggest" : {
+                        "prefix" : value,
+                        "completion" : {
+                            "field" : "name_suggest",
+                            "size": 10
+                        }
+                    }
+                }
+            })
+        };
+
+        fetch('/api/_search', requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({
+                suggestions: data.suggest["search-suggest"][0].options.map(item => item.text)
+            }));
+    };
+
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        });
     };
 
     render() {
+        const { value, suggestions } = this.state;
+
+        const inputProps = {
+            placeholder: 'Search for anything...',
+            id: "search",
+            type: "search",
+            value,
+            onChange: this.onChange,
+            onKeyDown: this.props.handleKeyDown,
+        };
+
         return (
-            <input
-                style={{ margin: '0 1rem' }}
-                id="search"
-                type="search"
-                placeholder="Search for anything..."
-                onKeyDown={this.props.handleKeyDown}
-                onChange={e => {
-                    this.setState({value: e.target.value});
-                    this.props.updateQueryText(e.target.value)
-                }}
+            <Autosuggest
+                suggestions={suggestions}
+                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                getSuggestionValue={getSuggestionValue}
+                renderSuggestion={renderSuggestion}
+                inputProps={inputProps}
             />
-        )
-    };
+        );
+    }
 }
